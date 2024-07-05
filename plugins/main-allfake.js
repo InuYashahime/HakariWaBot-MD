7b7b953ba1190f470d248aa169d7bc9da5dfc4ed
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-04 22:07:18 -0500

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -85,7 +85,7 @@ global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@
 
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: `${packname}`, orderTitle: 'Bang', thumbnail: icons, sellerJid: '0@s.whatsapp.net'}}}
 
-let icono = ['', ''].getRandom()
+let icono = ['https://telegra.ph/file/83b4d144aa349286364f5.jpg', 'https://telegra.ph/file/3e9ea97ffd9261ffae73f.jpg', 'https://telegra.ph/file/a48a77eaef244203a447b.jpg'].getRandom()
 
 global.rcanal = {
 contextInfo: {
