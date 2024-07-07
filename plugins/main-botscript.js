c4b1dd83cab5694b0de402257a17c75429bd04c3
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-06 20:59:46 -0500

    Actualizar main-botscript.js

--- plugins/main-botscript.js
+++ plugins/main-botscript.js
@@ -2,7 +2,7 @@ import moment from 'moment-timezone'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
-let res = await fetch('https://api.github.com/repos/OfcDiego/HakariWaBot-MD')
+let res = await fetch('https://api.github.com/repos/OfcDiego/HoshinoWaBot-MD')
 let json = await res.json()
 try {
 let txt = `*乂  S C R I P T  -  M A I N  乂*\n\n`
